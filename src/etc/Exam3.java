8f1a67021e999c5df0b4002c332ae434cd8094f9
clowncdi <clowncdi85@gmail.com>
2023-02-05 21:22:03 +0900

    add 배열의 요소들을 지그재그 재배열

---
+++ src/etc/Exam3.java
@@ -0,0 +1,40 @@
+package etc;
+
+import java.util.Arrays;
+
+/*
+* 배열의 요소들을 지그재그 재배열
+* 1,2,3
+* 6,5,4
+* 7,8,9
+*  , ,10
+*/
+public class Exam3 {
+  public static void main(String[] args) {
+    final int LASTNUM = 10;
+    final int MAXCOLCOUNT = 3;
+    int maxCount = LASTNUM / MAXCOLCOUNT + (LASTNUM % MAXCOLCOUNT > 0 ? 1 : 0);
+    Integer[] data = new Integer[LASTNUM];
+    for (int i = 0; i < LASTNUM; i++) {
+      data[i] = i + 1;
+    }
+    Integer[][] result = new Integer[maxCount][MAXCOLCOUNT];
+    int idx = 0;
+    for (int i = 0; i < maxCount; i++) {
+      if (i % 2 == 0) {
+        for (int j = 0; j < MAXCOLCOUNT; j++) {
+          if (idx < LASTNUM) {
+            result[i][j] = data[idx++];
+          }
+        }
+      } else {
+        for (int j = MAXCOLCOUNT - 1; j >= 0 ; j--) {
+          if (idx < LASTNUM) {
+            result[i][j] = data[idx++];
+          }
+        }
+      }
+    }
+    System.out.println(Arrays.deepToString(result));
+  }
+}
