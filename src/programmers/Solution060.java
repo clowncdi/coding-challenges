a1c9012e496b9bf0d126fc27b0fdb8c1ae1cfe52
clowncdi <clowncdi85@gmail.com>
2023-04-18 00:16:52 +0900

    add 프로그래머스

--- src/programmers/Solution060.java
+++ src/programmers/Solution060.java
@@ -3,38 +3,37 @@ package programmers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 실패율
  *
- * map 정렬
+ * map 역정렬
  */
 public class Solution060 {
   public static void main(String[] args) {
-    int N = 4;
-    int[] stages = {4,4,4,4,4};
+    int N = 10;
+    int[] stages = {3,5,5,3,3};
 
     int totalCount = stages.length;
-    HashMap<Integer, Double> map = new HashMap<>();
+    Map<Integer, Double> stageFailRate = new HashMap<>();
     for (int i = 1; i <= N; i++) {
-      map.put(i, 0.0);
+      stageFailRate.put(i, 0.0);
     }
 
-    for (int i = 0; i < stages.length; i++) {
-      if (stages[i] > N) {
-        continue;
-      }
-      map.put(stages[i], map.get(stages[i]) + 1);
+    for (int stage : stages) {
+      if (stage > N) continue;
+      stageFailRate.put(stage, stageFailRate.getOrDefault(stage, 0.0) + 1.0);
     }
 
-    for (Integer i : map.keySet()) {
-      Double minus = map.get(i);
-      map.put(i, map.get(i) / totalCount);
-      totalCount -= minus;
+    for (Integer key : stageFailRate.keySet()) {
+      Double count = stageFailRate.get(key);
+      stageFailRate.put(key, count == 0.0 ? 0.0 : count / (double) totalCount);
+      totalCount -= count;
     }
 
-    ArrayList<Integer> keySet = new ArrayList<>(map.keySet());
-    keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+    ArrayList<Integer> keySet = new ArrayList<>(stageFailRate.keySet());
+    keySet.sort((o1, o2) -> stageFailRate.get(o2).compareTo(stageFailRate.get(o1)));
 
     int[] array = keySet.stream().mapToInt(i -> i).toArray();
     System.out.println(Arrays.toString(array));
